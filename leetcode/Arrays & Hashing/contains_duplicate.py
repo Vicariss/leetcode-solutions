d7bc4dbfa80e429a90d389e7282b5553207935a3
Vicaris <vicarisfsc@gmail.com>
2022-08-28 00:46:00 +0200

    1/9 - Contains Duplicates

---
+++ leetcode/Arrays & Hashing/contains_duplicate.py
@@ -0,0 +1,9 @@
+# check if given list of integers contains duplicates
+
+hashset = set() 
+def duplicates(arr):
+    for num in arr:
+        if num in hashset:
+            return True
+        hashset.add(num)
+    return False
